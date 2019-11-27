04c774923e8cd5e0e10289b33d35475d5725cdca
unknown <meiling.dai@csiro.au>
2019-11-27 22:21:42 +1100

    save

--- project_summary/r proj.R
+++ project_summary/r proj.R
@@ -38,6 +38,50 @@ field_horses_raw_counts <- read_csv("data/field_horses_raw_counts.csv")
 field_horses_raw_counts
 
 
+#use geom_boxplot and geom_jitter to plot microRNA raw counts of field horses
+full_data %>%  ggplot (aes(x = horse_id,
+                           y = counts, 
+                           color = infection, 
+                           group = horse_id))+
+  geom_boxplot() + 
+  geom_jitter()+
+  scale_y_log10()+
+  scale_color_manual(values = c(mock = "grey", infected = "red")) +
+  theme_bw() +
+  labs(title = "microRNA raw counts of field horses")
+
+
+#use geom_boxplot and geom_jitter to plot microRNA raw counts of field horses with raw counts > 10,000.
+aw_count_more_than_10000 <- full_data %>% 
+  filter(counts > 10000)
+
+raw_count_more_than_10000 %>% ggplot(aes(x = horse_id,
+                                         y = counts, 
+                                         color = infection, 
+                                         group = horse_id))+
+  geom_boxplot() + 
+  geom_jitter()+
+  scale_y_log10()+
+  scale_color_manual(values = c(mock = "grey", infected = "red")) +
+  theme_bw() +
+  labs(title = "microRNA raw counts more than 1000 of 6 horses")
+
+
+#PCA analysis 
+field_horses_raw_counts_PCA <- field_horses_raw_counts %>% 
+  column_to_rownames("gene") %>% 
+  scale()
+
+# Peform the PCA on the already scaled heat_stress data
+microRNA_PCA <- prcomp(field_horses_raw_counts_PCA)
+
+# Check the summary
+summary(microRNA_PCA)
+plot(microRNA_PCA)
+
+
+
+
 
 data_1 <- field_horses_raw_counts %>% 
   filter(gene > 0) %>% 
@@ -61,22 +105,15 @@ data_without_gene
 # Store GeneID as rownames
 rownames(data_without_gene) <- field_horses_raw_counts$gene
 
+
+
+
+
 View(data_without_gene)
 head(data_without_gene)
 
 
 
-#PCA analysis 
-field_horses_raw_counts_PCA <- field_horses_raw_counts %>% 
-  column_to_rownames("gene") %>% 
-  scale()
 
-# Peform the PCA on the already scaled heat_stress data
-PCA <- prcomp(field_horses_raw_counts_PCA)
 
-# Check the summary
-summary(PCA)
 
-PC1_PC2 <- PCA$x %>% 
-  ggplot(aes(x = PC1, y = PC2)) +
-  geom_text()
